784119c935ed1cc84152b6e5b36d22de7e5fe4c8
Ana Heredero <ana.heredero.gomez@gmail.com>
2019-11-18 21:05:45 +0100

    Scripts added

--- scripts/index.sh
+++ scripts/index.sh
@@ -4,3 +4,13 @@
 # The STAR command is provided for you. You should replace the parts surrounded by "<>" and uncomment it.
 
 # STAR --runThreadN 4 --runMode genomeGenerate --genomeDir <outdir> --genomeFastaFiles <genomefile> --genomeSAindexNbases 9
+
+
+
+echo "Running STAR index..."
+mkdir -p res/genome/star_index
+STAR --runThreadN 4 --runMode genomeGenerate \
+--genomeDir res/genome/star_index/ \
+--genomeFastaFiles res/genome/ecoli.fasta \
+--genomeSAindexNbases 9
+
--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -1,3 +1,12 @@
 # This script should merge all files from a given sample (the sample id is provided in the third argument)
 # into a single file, which should be stored in the output directory specified by the second argument.
 # The directory containing the samples is indicated by the first argument.
+
+sid = $3
+dir = $1
+merged = $2
+
+for samples in $1
+do
+	cat 
+
