7bf9ba77248105ac304cc333fdc01fdb7218ef32
Ana Heredero <ana.heredero.gomez@gmail.com>
2019-11-20 19:09:34 +0100

    STAR script updated

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -20,24 +20,29 @@
 #done
 
 # TODO: run cutadapt for all merged files--> for 
-echo "Running cutadapt..."
-mkdir -p log/cutadapt
-mkdir -p out/trimmed
-for sid in $(ls out/merged/*.fastq.gz | cut -d "." -f1 | sed "s:out/merged/::" | sort | uniq)
-do
-cutadapt -m 18 -a TGGAATTCTCGGGTGCCAAGG --discard-untrimmed  \
--o out/trimmed/$sid.trimmed.fastq.gz out/merged/$sid.merged.fastq.gz > log/cutadapt/$sid.log
-done
+#echo "Running cutadapt..."
+#mkdir -p log/cutadapt
+#mkdir -p out/trimmed
+#for sid in $(ls out/merged/*.fastq.gz | cut -d "." -f1 | sed "s:out/merged/::" | sort | uniq)
+#do
+#cutadapt -m 18 -a TGGAATTCTCGGGTGCCAAGG --discard-untrimmed  \
+#-o out/trimmed/$sid.trimmed.fastq.gz out/merged/$sid.merged.fastq.gz > log/cutadapt/$sid.log
+#done
 
 
 #TODO: run STAR for all trimmed files
-#for fname in out/trimmed/*.fastq.gz
-#do
-    # you will need to obtain the sample ID from the filename
-#    sid=#TODO
-    # mkdir -p out/star/$sid
-    # STAR --runThreadN 4 --genomeDir res/contaminants_idx --outReadsUnmapped Fastx --readFilesIn <input_file> --readFilesCommand zcat --outFileNamePrefix <output_directory>
-#done 
+echo "Running STAR alignment for all trimmed files..."
+for fname in out/trimmed/*.fastq.gz
+do
+    	#you will need to obtain the sample ID from the filename
+    	sid=$(basename $fname .trimmed.fastq.gz)
+	mkdir -p out/star/${sid}
+	STAR --runThreadN 4 --genomeDir res/contaminants_idx \
+	--outReadsUnmapped Fastx \
+	--readFilesIn out/trimmed/${sid}.trimmed.fastq.gz \
+	--readFilesCommand zcat \
+	--outFileNamePrefix out/star/${sid}. 
+done 
 
 # TODO: create a log file containing information from cutadapt and star logs
 # (this should be a single log file, and information should be *appended* to it on each run)
