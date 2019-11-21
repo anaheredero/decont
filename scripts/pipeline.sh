8c3f26b69564d5a80dcd0aed94c5c84a3e152405
Ana Heredero <ana.heredero.gomez@gmail.com>
2019-11-21 21:13:37 +0100

    Log script completed and added

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -31,20 +31,33 @@
 
 
 #TODO: run STAR for all trimmed files
-echo "Running STAR alignment for all trimmed files..."
-for fname in out/trimmed/*.fastq.gz
-do
+#echo "Running STAR alignment for all trimmed files..."
+#for fname in out/trimmed/*.fastq.gz
+#do
     	#you will need to obtain the sample ID from the filename
-    	sid=$(basename $fname .trimmed.fastq.gz)
-	mkdir -p out/star/${sid}
-	STAR --runThreadN 4 --genomeDir res/contaminants_idx \
-	--outReadsUnmapped Fastx \
-	--readFilesIn out/trimmed/${sid}.trimmed.fastq.gz \
-	--readFilesCommand zcat \
-	--outFileNamePrefix out/star/${sid}. 
-done 
+#    	sid=$(basename $fname .trimmed.fastq.gz)
+#	mkdir -p out/star/${sid}
+#	STAR --runThreadN 4 --genomeDir res/contaminants_idx \
+#	--outReadsUnmapped Fastx \
+#	--readFilesIn out/trimmed/${sid}.trimmed.fastq.gz \
+#	--readFilesCommand zcat \
+#	--outFileNamePrefix out/star/${sid}/${sid}. 
+#done 
 
 # TODO: create a log file containing information from cutadapt and star logs
 # (this should be a single log file, and information should be *appended* to it on each run)
 # - cutadapt: Reads with adapters and total basepairs
 # - star: Percentages of uniquely mapped reads, reads mapped to multiple loci, and to too many loci
+
+echo "Creating the final report..."
+for sid in $(ls data/*.fastq.gz | cut -d "-" -f1 | sed "s:data/::" | sort | uniq)
+do
+	echo "			~ ${sid} ~" >> log/pipeline.log
+	echo >> log/pipeline.log
+	cat log/cutadapt/${sid}.log | grep "Reads with adapters:" >> log/pipeline.log
+	cat log/cutadapt/${sid}.log | grep "Total basepairs processed" >> log/pipeline.log
+	cat out/star/${sid}/${sid}.Log.final.out | grep "Uniquely mapped reads %" >> log/pipeline.log
+	cat out/star/${sid}/${sid}.Log.final.out | grep "% of reads mapped to multiple loci" >> log/pipeline.log
+	cat out/star/${sid}/${sid}.Log.final.out | grep "% of reads mapped to too many loci" >> log/pipeline.log
+	echo >> log/pipeline.log
+done
